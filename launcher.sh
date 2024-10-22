2bf58e111dcd303adb4110ee810b715d2f6edff8
Nizamuddin Mohamed <webnizam@gmail.com>
2024-10-22 21:49:10 +0400

    added system service

--- launcher.sh
+++ launcher.sh
@@ -2,27 +2,26 @@
 
 # Define the working directory for the GPS tracker
 SCRIPT_DIR="/data/openpilot/traccar-python-client"
-OPENPILOT_DIR="/data/openpilot"  # The directory where the OpenPilot code is located
+OPENPILOT_DIR="/data/openpilot" # The directory where the OpenPilot code is located
 
 # Path to the Python script
 SCRIPT_PATH="$SCRIPT_DIR/gps_tracker.py"
 
-# Activate virtual environment (if using one)
-# source $SCRIPT_DIR/venv/bin/activate
+# Specify the Python path
+PYTHON_PATH="/usr/local/pyenv/shims/python3"
 
 # Add openpilot directory to PYTHONPATH so that the script can find the 'cereal' package
 export PYTHONPATH="$OPENPILOT_DIR:$PYTHONPATH"
 
-# Ensure all dependencies are installed
-pip install -r $SCRIPT_DIR/requirements.txt
+# Ensure all dependencies are installed using the correct Python interpreter
+$PYTHON_PATH -m pip install -r $SCRIPT_DIR/requirements.txt
 
 # Check if the process is already running
-if pgrep -f $SCRIPT_PATH > /dev/null
-then
+if pgrep -f $SCRIPT_PATH >/dev/null; then
     echo "GPS Tracker is already running."
 else
     echo "Starting GPS Tracker..."
-    # Run the Python script (logging will be handled by systemd)
-    python3 $SCRIPT_PATH
+    # Run the Python script with the specified Python interpreter
+    $PYTHON_PATH $SCRIPT_PATH
     echo "GPS Tracker started."
 fi
